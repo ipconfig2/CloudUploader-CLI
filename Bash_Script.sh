4ab0de8642ddcd2749afa262c810154f4575852a
Holden Weber <78152356+ipconfig2@users.noreply.github.com>
2024-02-08 08:21:54 -0500

    Update Bash_Script.sh

--- Bash_Script.sh
+++ Bash_Script.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# A script that will will enable users to upload files via the cli
+
+setup() { 
+    # Install az cli
+    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+    # Login
+    az login --use-device-code
+    echo "You're logged in."
+}
+
+
+
+setup
