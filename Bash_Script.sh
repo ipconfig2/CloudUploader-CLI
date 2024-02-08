a2df9fc4316590abac975cace6d1013d13a4ce60
Holden Weber <78152356+ipconfig2@users.noreply.github.com>
2024-02-08 10:53:11 -0500

    Update Bash_Script.sh

--- Bash_Script.sh
+++ Bash_Script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# A script that will will enable users to upload files via the cli
+# A script that will enable users to upload files via the CLI
 
-setup() { 
+Authentication() { 
     # Install az cli
     curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
     # Login
@@ -10,6 +10,76 @@ setup() {
     echo "You're logged in."
 }
 
+ResourceGRP(){
+
+# Print out 5 recommended regions
+print_out_regions() {
+    regions_array=($( az account list-locations --query "[?metadata.regionCategory=='Recommended'].{Name:name}" -o tsv | head -n 5))
+    for i in "${regions_array[@]}"
+    do
+       echo "$i"
+    done
+}
+
+# Select a region Function
+check_region() {
+    local region_exists=false
+    while [[ "$region_exists" = false ]];  do
+        print_out_regions
+        read -p "Enter your region: " selected_region
+        for j in "${regions_array[@]}"
+        do
+            if [[ "$selected_region" == "$j" ]]; then
+                region_exists=true
+                echo "Region exists"
+                break
+            else
+                continue
+            fi
+        done
+    done
+}
+
+    # Check if resource group already exists.
+check_resource_group () {
+    while true; do
+        read -p "Enter a name for you resource group: " resource_group
+        if [ $(az group exists --name $resource_group) = true ]; then 
+            echo "The group $resource_group exists in $selected_region, please provide another name..."
+        else
+            break
+        fi
+    done
+}
+
+# Create the resource group
+create_resource_group () {
+    echo "Creating resource group: $resource_group in $selected_region"
+    az group create -g $resource_group -l $selected_region | grep provisioningState
+}
+
+#List all resource groups
+list_resource_groups() {
+    az group list -o table
+}
+
+}
+    #Prompt User
+    echo "Would you like to create a new resource GRP?"
+    read answer
+        if [ "$answer" = "yes"] || ["$answer" = "y"];
+            then {
+                print_out_regions
+                check_region
+                create_resource_group
+                list_resource_groups
+            }
+            else{
+                echo "OK We will make a Resource Group"
+            }
+}
 
 
-setup
+
+Authentication
+ResourceGRP
